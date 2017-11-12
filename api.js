3ae05f61cad9184be2272e8fcfc32149dd2abb2f
Karol Walasek <coreconviction@gmail.com>
2017-11-12 16:13:12 +0100

    Ania: Dodane transformowanie obiektów, dodane nowe bloki

--- api.js
+++ api.js
@@ -18,17 +18,29 @@ function getCodeForType(type, block, name){
 	}
 }
 
+function stripQuotes(str){
+	console.log('strip '+str);
+	if(str && str.substr(0) == '"' && str.substr(-1) == '"')
+		return str.substr(1, str.length-2);
+	return str;
+}
+
+function ObjectStringifyNoquotes(obj){
+	return '{'+Object.keys(obj).map((k) => `"${k}": ${obj[k]}`)+'}';
+}
+
 function loadBlockDefinitions(){
 	BLOCK_DEFS.forEach((def) => {
 		Blockly.Blocks[def.name] = {
 			init: function(){
 				this.setInputsInline(true);
-				this.setColour(160);
+				this.setColour(def.transform ? 40 : 160);
 				this.setNextStatement(true);
 				this.setPreviousStatement(true);
 				this.appendDummyInput()
 					.appendField(def.name)
 				def.attributes.forEach((attr) => {
+					console.log(attr);
 					var f = null;
 					switch(attr.type){
 						case 'value':
@@ -48,7 +60,18 @@ function loadBlockDefinitions(){
 			def.attributes.forEach((attr) => {
 				attributes_resolved[attr.name] = getCodeForType(attr.type, block, attr.name) || attr.default || defaultForType(attr.type);
 			});
-			var spawn_code = `addToScene(${def.create.toString()}, (obj) => {${attributes_resolved.initialization}}, (obj) => {${attributes_resolved.animation}}, ${JSON.stringify(attributes_resolved)});`;
+			var spawn_code;
+			if(def.transform){
+				return `if(obj)(${def.transform.toString()})(obj, ${ObjectStringifyNoquotes(attributes_resolved)});`;
+			}else{
+				spawn_code = `
+				addToScene(
+					${def.create.toString()},
+					(obj) => {${attributes_resolved.initialization || ''}},
+					(obj) => {${attributes_resolved.animation || ''}},
+					${JSON.stringify(attributes_resolved) || null}
+				);`;
+			}
 			return spawn_code;
 		}
 	});
--- blocks.js
+++ blocks.js
@@ -2,11 +2,44 @@ addObjectDefinition({
 	name: 'Cube',
 	attributes: [
 		{name: 'size', type: 'value', default: 1},
-		{name: 'initialization', type: 'statement', default: 'obj.position.x = Math.random()*2-1;obj.position.y = Math.random()*2-1;obj.position.z = Math.random()*2-1;'},
-		{name: 'animation', type: 'statement', default: 'obj.rotation.x += 0.01; obj.rotation.y += 0.02;'}
+		{name: 'initialization', type: 'statement'},
+		{name: 'animation', type: 'statement'}
 	],
 	create: function(graphics, attributes){
 		return new graphics.Mesh(graphics.BoxGeometry(attributes.size,attributes.size,attributes.size), graphics.MeshBasicMaterial({color: 0xFF0000}));
+	}
+});
+
+addObjectDefinition({
+	name: 'Rotate',
+	attributes: [
+		{name: 'rx', type: 'value', default: 0},
+		{name: 'ry', type: 'value', default: 0},
+		{name: 'rz', type: 'value', default: 0}
+	],
+	transform: function(object, attributes){
+		if(object){
+			object.rotateX((attributes.rx));
+			object.rotateY((attributes.ry));
+			object.rotateZ((attributes.rz));
+		}
+	}
+});
+
+addObjectDefinition({
+	name: 'Translate',
+	attributes: [
+		{name: 'dx', type: 'value', default: 0},
+		{name: 'dy', type: 'value', default: 0},
+		{name: 'dz', type: 'value', default: 0}
+	],
+	transform: function(object, attributes){
+		if(object){
+			object.position.x += (attributes.dx);
+			object.position.y += (attributes.dy);
+			object.position.z += (attributes.dz);
+		}
+		//console.log('Moving by '+attributes.rx);
 	}
 });
 
